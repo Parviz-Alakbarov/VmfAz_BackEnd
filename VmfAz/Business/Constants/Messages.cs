4a5740e8be3e76f2f30072d5805f53ef43b12141
Parviz-Alakbarov <parvizja@code.edu.az>
2022-02-28 07:43:05 +0400

    Result feature implemented

---
+++ VmfAz/Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        //Product Messages
+        public static readonly string ProductAdded = "Product Added Successfully!";
+
+        //Brand Messages
+        public static readonly string BrandAdded = "Brand Added Successfully!";
+    }
+}
