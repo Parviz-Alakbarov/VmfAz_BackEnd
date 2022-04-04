aeeb085110c171040412c719c6504790dbe2aa7c
Parviz-Alakbarov <parvizja@code.edu.az>
2022-04-04 16:07:46 +0400

    Exception Handler Added

--- VmfAz/WebAPI/Startup.cs
+++ VmfAz/WebAPI/Startup.cs
@@ -99,7 +99,10 @@ namespace WebAPI
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
 
+            app.ConfigureCustomExceptionMiddleware();
+
             app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4201").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
